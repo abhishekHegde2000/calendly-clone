41ae04ea12d45d1d647c969d0ac2da1516e5f3a4
Abhishek Hegde <ahegde2021@gmail.com>
2024-10-10 18:46:08 +0530

    prettier and eslint added

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -17,6 +17,7 @@ module.exports = {
     "^@/hooks/(.*)$",
     "^@/components/ui/(.*)$",
     "^@/components/(.*)$",
+    "^@/registry/(.*)$",
     "^@/styles/(.*)$",
     "^@/app/(.*)$",
     "",
@@ -28,5 +29,8 @@ module.exports = {
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
   importOrderMergeDuplicateImports: true,
   importOrderCombineTypeAndValueImports: true,
-  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
 }
